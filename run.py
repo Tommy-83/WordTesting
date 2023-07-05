5ce7b1e8d53a46a47e2a59a16f83fda036f9f140
Tommy-83 <tommy.kosakowski@gmail.com>
2023-07-05 16:31:56 +0000

    add

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
 import random
-import time
 from datetime import datetime, timedelta
-from termcolor import colored
 
 app = Flask(__name__)
 
@@ -10,7 +8,6 @@ app = Flask(__name__)
 WORD_LENGTH = 5
 MAX_ATTEMPTS = 6
 TIMER_DURATION = 180  # 3 minutes in seconds
-
 
 # Function to generate a random word
 def generate_word():
@@ -23,28 +20,6 @@ def generate_word():
                  "wrist", "plaza", "prize", "swamp", "glory", "stool", "angel", "ocean", "black", "stair",
                  "shirt", "chair", "craft", "shade", "steel", "roast", "order", "proud", "thief", "broom"]
     return random.choice(word_list)
-
-
-# Function to display the game grid
-def display_grid(grid, elapsed_time, reveal_word):
-    print("    " + " ".join(colored(chr(65+i), 'white') for i in range(WORD_LENGTH)))
-    print("  +" + "---+" * WORD_LENGTH)
-
-    for row in grid:
-        colored_row = []
-        for letter in row:
-            if letter['visible']:
-                colored_row.append(colored(letter['letter'], letter['color'], 'on_grey'))
-            else:
-                colored_row.append(colored("_", 'white', 'on_grey'))
-        print("  | " + " | ".join(colored_row) + " |")
-        print("  +" + "---+" * WORD_LENGTH)
-
-    print()
-    print("Time Elapsed: {} minutes {} seconds".format(elapsed_time.seconds // 60, elapsed_time.seconds % 60))
-
-    if reveal_word:
-        print("The word was:", reveal_word)
 
 
 # Function to check the correctness of the guess
@@ -60,93 +35,47 @@ def check_guess(guess, target_word):
     return feedback
 
 
-# Function to play the game
-def play_game():
-    target_word = generate_word()
-    attempts = 0
-    grid = []
-    start_time = datetime.now()
-    end_time = start_time + timedelta(seconds=TIMER_DURATION)
-
-    print("Welcome to Wordle!")
-    print("Guess the five-letter word.")
-    print("You have", MAX_ATTEMPTS, "attempts.")
-    print("You have 3 minutes to complete the game.")
-
-    while attempts < MAX_ATTEMPTS and datetime.now() <= end_time:
-        elapsed_time = datetime.now() - start_time
-        display_grid(grid, elapsed_time, None)
-        guess = input("Enter your guess: ").lower()
-
-        if len(guess) != WORD_LENGTH:
-            print("Invalid guess. Guess should be", WORD_LENGTH, "letters long.")
-            continue
-
-        feedback = check_guess(guess, target_word)
-        grid.append(feedback)
-
-        if feedback == [{'letter': guess[i], 'color': 'green', 'visible': True} for i in range(WORD_LENGTH)]:
-            elapsed_time = datetime.now() - start_time
-            display_grid(grid, elapsed_time, target_word)
-            print("Congratulations! You guessed the word.")
-            break
-
-        attempts += 1
-
-    elapsed_time = datetime.now() - start_time
-    if attempts == MAX_ATTEMPTS:
-        display_grid(grid, elapsed_time, target_word)
-        print("Sorry, you have used all your attempts.")
-    elif datetime.now() > end_time:
-        display_grid(grid, elapsed_time, target_word)
-        print("Sorry, you ran out of time.")
-
-    play_again = input("Do you want to play again? (yes/no): ")
-    if play_again.lower() == 'yes':
-        play_game()
-    else:
-        print("Thank you for playing Wordle!")
-
-
+# Route for the home page
 @app.route('/', methods=['GET', 'POST'])
-def wordle():
+def play_game():
     if request.method == 'POST':
+        guess = request.form['guess'].lower()
+
+        # Validate the guess
+        if len(guess) != WORD_LENGTH:
+            return render_template('index.html', message='Invalid guess. Guess should be {} letters long.'.format(WORD_LENGTH))
+
+        # Play the game
         target_word = generate_word()
-        attempts = 0
         grid = []
+        attempts = 0
         start_time = datetime.now()
         end_time = start_time + timedelta(seconds=TIMER_DURATION)
 
         while attempts < MAX_ATTEMPTS and datetime.now() <= end_time:
             elapsed_time = datetime.now() - start_time
 
-            guess = request.form['guess'].lower()
-
-            if not guess:
-                message = "Please enter a valid guess."
-                return render_template('index.html', message=message)
-
-            if len(guess) != WORD_LENGTH:
-                message = "Invalid guess. Guess should be {} letters long.".format(WORD_LENGTH)
-                return render_template('index.html', message=message)
-
             feedback = check_guess(guess, target_word)
             grid.append(feedback)
 
             if feedback == [{'letter': guess[i], 'color': 'green', 'visible': True} for i in range(WORD_LENGTH)]:
                 elapsed_time = datetime.now() - start_time
-                return render_template('result.html', message="Congratulations! You guessed the word.", grid=grid, elapsed_time=elapsed_time, reveal_word=target_word)
+                return render_template('index.html', grid=grid, elapsed_time=elapsed_time, target_word=target_word,
+                                       message="Congratulations! You guessed the word.")
 
             attempts += 1
 
         elapsed_time = datetime.now() - start_time
         if attempts == MAX_ATTEMPTS:
-            return render_template('result.html', message="Sorry, you have used all your attempts.", grid=grid, elapsed_time=elapsed_time, reveal_word=target_word)
+            return render_template('index.html', grid=grid, elapsed_time=elapsed_time, target_word=target_word,
+                                   message="Sorry, you have used all your attempts.")
         elif datetime.now() > end_time:
-            return render_template('result.html', message="Sorry, you ran out of time.", grid=grid, elapsed_time=elapsed_time, reveal_word=target_word)
+            return render_template('index.html', grid=grid, elapsed_time=elapsed_time, target_word=target_word,
+                                   message="Sorry, you ran out of time.")
 
+    # Render the initial page
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
