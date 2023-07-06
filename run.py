d981071784e57a855b3df8af901a3488ca40d9cb
Tommy-83 <tommy.kosakowski@gmail.com>
2023-07-06 11:47:03 +0000

    adding words

--- run.py
+++ run.py
@@ -1,91 +1,45 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import random
-import os
-from datetime import datetime, timedelta
-from termcolor import colored
 
 app = Flask(__name__)
 
-# Constants
-WORD_LENGTH = 5
-MAX_ATTEMPTS = 6
-TIMER_DURATION = 180  # 3 minutes in seconds
+words = [
+    "apple", "beach", "camel", "dance", "eagle",  # Add more words here...
+]
 
-# Function to generate a random word
-def generate_word():
-    word_list = ["stove", "grape", "apple", "honey", "flame", "table", "fence", "lucky", "scent", "novel",
-                 "chess", "frost", "wrist", "juice", "waste", "virus", "sweep", "beard", "shaft", "shock",
-                 "brick", "beast", "sight", "curse", "treat", "charm", "yield", "frown", "horse", "spoil",
-                 "bless", "bonus", "queen", "river", "sweat", "braid", "grasp", "blend", "cliff", "crane",
-                 "shelf", "doubt", "rider", "bunch", "nurse", "vital", "tiger", "trick", "wool", "cabin",
-                 "flash", "sweep", "grill", "drain", "blame", "sting", "curve", "lemon", "spear", "steak",
-                 "wrist", "plaza", "prize", "swamp", "glory", "stool", "angel", "ocean", "black", "stair",
-                 "shirt", "chair", "craft", "shade", "steel", "roast", "order", "proud", "thief", "broom"]
-    return random.choice(word_list)
+target_word = random.choice(words)
+attempts = 0
+guessed_words = []
 
 
-# Function to check the correctness of the guess
-def check_guess(guess, target_word):
-    feedback = []
-    for i in range(WORD_LENGTH):
-        if guess[i] == target_word[i]:
-            feedback.append({'letter': guess[i], 'color': 'green', 'visible': True})  # Correct letter in the correct position
-        elif guess[i] in target_word:
-            feedback.append({'letter': guess[i], 'color': 'yellow', 'visible': True})  # Correct letter in the wrong position
-        else:
-            feedback.append({'letter': guess[i], 'color': 'red', 'visible': True})  # Incorrect letter
-    return feedback
-
-
-# Route for the home page
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', attempts=attempts, guessed_words=guessed_words)
 
 
-# Route for playing the game
-@app.route('/play', methods=['POST'])
-def play():
-    target_word = generate_word()
-    attempts = 0
-    grid = []
-    start_time = datetime.now()
-    end_time = start_time + timedelta(seconds=TIMER_DURATION)
+@app.route('/check_word', methods=['POST'])
+def check_word():
+    global attempts
+    global target_word
+    word = request.form['word'].lower()
+    attempts += 1
+    response = {'missed': 0, 'wrong_placed': 0, 'correct': 0}
 
-    while attempts < MAX_ATTEMPTS and datetime.now() <= end_time:
-        elapsed_time = datetime.now() - start_time
-        guess = request.form['guess'].lower()
+    if len(word) != 5:
+        return "Please enter a 5-letter word."
 
-        if len(guess) != WORD_LENGTH:
-            return jsonify({'message': 'Invalid guess. Guess should be {} letters long.'.format(WORD_LENGTH)})
+    for i, char in enumerate(word):
+        if char in target_word:
+            if char == target_word[i]:
+                response['correct'] += 1
+            else:
+                response['wrong_placed'] += 1
+        else:
+            response['missed'] += 1
 
-        feedback = check_guess(guess, target_word)
-        grid.append(feedback)
-
-        if feedback == [{'letter': guess[i], 'color': 'green', 'visible': True} for i in range(WORD_LENGTH)]:
-            elapsed_time = datetime.now() - start_time
-            return jsonify({
-                'message': 'Congratulations! You guessed the word.',
-                'grid': grid,
-                'elapsed_time': elapsed_time.seconds
-            })
-
-        attempts += 1
-
-    elapsed_time = datetime.now() - start_time
-    if attempts == MAX_ATTEMPTS:
-        return jsonify({
-            'message': 'Sorry, you have used all your attempts.',
-            'grid': grid,
-            'elapsed_time': elapsed_time.seconds
-        })
-    elif datetime.now() > end_time:
-        return jsonify({
-            'message': 'Sorry, you ran out of time.',
-            'grid': grid,
-            'elapsed_time': elapsed_time.seconds
-        })
+    guessed_words.append((word, response))
+    return render_template('index.html', attempts=attempts, guessed_words=guessed_words)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
